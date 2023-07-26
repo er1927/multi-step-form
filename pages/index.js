b8ae60768bb7b1d658b29eb46d37f1ad279d6718
elizabeth_roberts <elizabeth.roberts1927@gmail.com>
2023-07-26 16:12:31 -0300

    Added basic layout step 1.

--- pages/index.js
+++ pages/index.js
@@ -7,25 +7,23 @@ export default function Home() {
       <Head>
         <title>MULTI STEP FORM</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="/node_modules/multi-step-form-js/css/multi-step-form.css" type="text/css"></link>
+        <link rel="stylesheet" href="" type="text/css"></link>
       </Head>
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">MULTI STEP FORM!</a>
-    </h1>
     <form class="msf">
     <div class="msf-header">
         <div class="row text-center">
-            <div class="msf-step col-md-4"><i class="fa fa-clipboard"></i> <span>Step 1</span></div>
-            <div class="msf-step col-md-4"><i class="fa fa-credit-card"></i><span>Step 2</span></div>
-            <div class="msf-step col-md-4"><i class="fa fa-check"></i> <span>Step 3</span></div>
-            <div class="msf-step col-md-4"><i class=""></i> <span>Step 4</span></div>
+            <div class="msf-step col-md-4"><span>Step 1</span></div>
+            <div class="msf-step col-md-4"><span>Step 2</span></div>
+            <div class="msf-step col-md-4"><span>Step 3</span></div>
+            <div class="msf-step col-md-4"><span>Step 4</span></div>
         </div>
     </div>
  
     <div class="msf-content">
         <div class="msf-view">
-            ...
+            <h1>Personal info</h1>
+            <h4>Please provide your name, email address, and phone number.</h4>
         </div>
         <div class="msf-view">
             ...
@@ -52,57 +50,6 @@ export default function Home() {
       <footer>
        
       </footer>
-
-      {/* <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style> */}
     </div>
   )
 }
