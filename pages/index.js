ef669ed6edee2815895c7448e51d50bd3c5a76a0
elizabeth_roberts <elizabeth.roberts1927@gmail.com>
2023-08-02 16:45:20 -0300

    Specified font weight.

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css';
 import { Ubuntu } from 'next/font/google'
 
 const ubuntu = Ubuntu({ 
-  weight: '400',
+  weight: ['400', '500', '700'],
   subsets: ['latin-ext'] })
 
 export default function Home() {
