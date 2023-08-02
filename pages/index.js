88bf92a5117b30046c6dd32e7715ad60fc589533
elizabeth_roberts <elizabeth.roberts1927@gmail.com>
2023-08-02 16:21:12 -0300

    Fixed wrong labeling from npm -from class to className

--- pages/index.js
+++ pages/index.js
@@ -10,21 +10,21 @@ export default function Home() {
         <link rel="stylesheet" href="" type="text/css"></link>
       </Head>
       <main>
-    <form class="msf">
-    <div class="msf-header">
-        <div class="row text-center">
-            <div class="msf-step col-md-4"><span>Step 1</span></div>
-            <div class="msf-step col-md-4"><span>Step 2</span></div>
-            <div class="msf-step col-md-4"><span>Step 3</span></div>
-            <div class="msf-step col-md-4"><span>Step 4</span></div>
+    <form className="msf">
+    <div className="msf-header">
+        <div className="row text-center">
+            <div className="msf-step col-md-4"><span>Step 1</span></div>
+            <div className="msf-step col-md-4"><span>Step 2</span></div>
+            <div className="msf-step col-md-4"><span>Step 3</span></div>
+            <div className="msf-step col-md-4"><span>Step 4</span></div>
         </div>
     </div>
  
-    <div class="msf-content">
-        <div class="msf-view">
+    <div className="msf-content">
+        <div className="msf-view">
             <h1>Personal info</h1>
             <h4>Please provide your name, email address, and phone number.</h4>
-            <div class="personal-info">
+            <div className="personal-info">
               <label>Name</label>
               <input placeholder="e.g. Alex Smith"/>
               <label>Email Address</label>
@@ -33,64 +33,64 @@ export default function Home() {
               <input type="number" placeholder="e.g. + 1 234 567 890"/>
             </div>
         </div>
-        <div class="msf-view">
+        <div className="msf-view">
         <h1>Select your plan</h1>
         <h4>You have the option of monthly or yearly billing.</h4>
-        <div class="select-plan">
+        <div className="select-plan">
             {/* toggle monthly/yearly here */}
          </div>
         </div>
-        <div class="msf-view">
+        <div className="msf-view">
         <h1>Pick add-ons</h1>
         <h4>Add-ons help enhance your gaming experience.</h4>
-        <div class="add-ons">
-          <div class="add-on">
-            <p class="title">Online service</p>
-            <p class="subtitle">Access to multiplayer games.</p>
-            <p class="price">+1$/mo</p>
+        <div className="add-ons">
+          <div className="add-on">
+            <p className="title">Online service</p>
+            <p className="subtitle">Access to multiplayer games.</p>
+            <p className="price">+1$/mo</p>
           </div>
-          <div class="add-on">
-            <p class="title">Larger storage</p>
-            <p class="subtitle">Extra 1TB of cloud save</p>
-            <p class="price">+2$/mo</p>
+          <div className="add-on">
+            <p className="title">Larger storage</p>
+            <p className="subtitle">Extra 1TB of cloud save</p>
+            <p className="price">+2$/mo</p>
           </div>
-          <div class="add-on">
-            <p class="title">Customizable Profile</p>
-            <p class="subtitle">Custom theme on your profile</p>
-            <p class="price">+2$/mo</p>
+          <div className="add-on">
+            <p className="title">Customizable Profile</p>
+            <p className="subtitle">Custom theme on your profile</p>
+            <p className="price">+2$/mo</p>
           </div>
         </div>
         </div>
-        <div class="msf-view">
+        <div className="msf-view">
         <h1>Finishing up</h1>
         <h4>Double-check everything looks OK before confirming.</h4>
-        <div class="final-tally">
-          <div class="tally-item">
-            <p class="chosen-plan-title"></p>
-            <p class="chosen-plan-price"></p>
+        <div className="final-tally">
+          <div className="tally-item">
+            <p className="chosen-plan-title"></p>
+            <p className="chosen-plan-price"></p>
           </div>
 
-          <div class="tally-item">
-            <p class="chosen-add-ons"></p>
-            <p class="chosen-add-ons-price"></p>
+          <div className="tally-item">
+            <p className="chosen-add-ons"></p>
+            <p className="chosen-add-ons-price"></p>
           </div>
 
-          <div class="tally-item">
-            <p class="total"></p>
-            <p class="total-amoung"></p>
+          <div className="tally-item">
+            <p className="total"></p>
+            <p className="total-amoung"></p>
           </div>
         </div>
       </div>
     </div>
  
-    <div class="msf-navigation">
-        <div class="row">
-            <div class="col-md-3">
-                <button type="button" data-type="back" class="btn btn-outline-dark msf-nav-button"><i class="fa fa-chevron-left"></i> Go back </button>
+    <div className="msf-navigation">
+        <div className="row">
+            <div className="col-md-3">
+                <button type="button" data-type="back" className="btn btn-outline-dark msf-nav-button"><i className="fa fa-chevron-left"></i> Go back </button>
             </div>
-            <div class="col-md-3 col-md-offset-6">
-                <button type="button" data-type="next" class="btn  btn-outline-dark msf-nav-button">Next Step<i class="fa fa-chevron-right"></i></button>
-                <button type="submit" data-type="submit" class="btn btn-outline-dark msf-nav-button">Confirm</button>
+            <div className="col-md-3 col-md-offset-6">
+                <button type="button" data-type="next" className="btn  btn-outline-dark msf-nav-button">Next Step<i className="fa fa-chevron-right"></i></button>
+                <button type="submit" data-type="submit" className="btn btn-outline-dark msf-nav-button">Confirm</button>
             </div>
         </div>
     </div>
